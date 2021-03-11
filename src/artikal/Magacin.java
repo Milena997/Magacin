9af5938db775f4405cbb187996ae3b45fd90c3cd
Milena997 <milenacosovic24@gmail.com>
2021-03-11 13:51:27 +0100

    Dodata klasa Magacin

---
+++ src/artikal/Magacin.java
@@ -0,0 +1,25 @@
+package artikal;
+
+import interfaceMagacin.InterfaceMagacin;
+
+public class Magacin implements InterfaceMagacin {
+
+	@Override
+	public void dodajArtikal(Artikal artikal) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void izbaciArtikal(Artikal artikal) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Artikal nadjiArtikal(int sifra) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
