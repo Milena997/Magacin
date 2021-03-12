6131ef46ebb3dbf61ae460d37e0e360f73fbbf25
Milena997 <milenacosovic24@gmail.com>
2021-03-12 19:02:47 +0100

    Implementirane metode u klasi Magacin na drugi nacin na master grani

--- src/artikal/Magacin.java
+++ src/artikal/Magacin.java
@@ -1,25 +1,42 @@
 package artikal;
 
+import java.util.LinkedList;
+
 import interfaceMagacin.InterfaceMagacin;
 
 public class Magacin implements InterfaceMagacin {
 
+	public LinkedList<Artikal> artikli= new LinkedList<>();
 	@Override
 	public void dodajArtikal(Artikal artikal) {
-		// TODO Auto-generated method stub
+		if(artikal!=null) {
+		artikli.add(artikal);
+		}
 		
 	}
 
 	@Override
 	public void izbaciArtikal(Artikal artikal) {
-		// TODO Auto-generated method stub
+		if(artikal!=null) {
+		artikli.remove(artikal);
+		}
 		
 	}
 
 	@Override
 	public Artikal nadjiArtikal(int sifra) {
-		// TODO Auto-generated method stub
+		if(sifra>0) {
+		
+		for(Artikal artikal: artikli) {
+			
+			if(artikal.getSifra()==sifra) {
+				return artikal;
+			}
+			
+		}
+		}
 		return null;
 	}
+
 
 }
